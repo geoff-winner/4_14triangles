a6f64ad1f4de89251d5d0937638d3f958781d4bb
Geoff Winner <Geoff@Geoffs-MacBook-Pro.local>
2015-04-10 08:08:09 -0700

    add scalene functions and tests

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var triangulator = function(input1, input2, input3) {
     var isosceles = 'Isosceles';
     var equilateral = 'Equilateral';
     var scalene = 'Scalene';
+    var notatriangle = 'Not a triangle';
 
     if (input1 === input2) {
       return isosceles;
@@ -9,6 +10,10 @@ var triangulator = function(input1, input2, input3) {
       return isosceles;
     } else if (input2 === input3) {
       return isosceles;
+    };
+
+    if (input1 !== input2 && input2 !== input3 && input3 !== input1) {
+      return scalene;
     };
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -9,4 +9,7 @@ describe('triangulator', function(){
     it("returns isosceles if input2 === input3", function(){
         expect(triangulator(2, 3, 3)).to.equal('Isosceles');
     });
+    it("returns not a triangle if input2 !== input3, input1 !== input2/3", function(){
+        expect(triangulator(2, 3, 4)).to.equal('Not a triangle');
+    });
 });
