9d476b6e41335cd17a0efaec932822a1348a1c6f
Geoff Winner <Geoff@Geoffs-MacBook-Pro.local>
2015-04-10 08:01:22 -0700

    add isosceles tests and if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 var triangulator = function(input1, input2, input3) {
+    var isosceles = 'Isosceles';
+    var equilateral = 'Equilateral';
+    var scalene = 'Scalene';
 
-}
+    if (input1 === input2) {
+      return isosceles;
+    } else if (input1 === input3) {
+      return isosceles;
+    } else if (input2 === input3) {
+      return isosceles;
+    };
+};
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,12 @@
-describe('scrabbler', function(){
+describe('triangulator', function(){
 
-    it("returns 1 for an entry of the word 'A'.", function(){
-        expect(scrabbler("A")).to.equal(1);
+    it("returns isosceles if input1 === input2", function(){
+        expect(triangulator(1, 1)).to.equal('Isosceles');
+    });
+    it("returns isosceles if input1 === input3", function(){
+        expect(triangulator(2, 1, 2)).to.equal('Isosceles');
+    });
+    it("returns isosceles if input2 === input3", function(){
+        expect(triangulator(2, 3, 3)).to.equal('Isosceles');
     });
 });
